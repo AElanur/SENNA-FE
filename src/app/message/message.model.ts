7357270fcca4916720f14b4f4c5ac9a811d0aa0d
Ahsen <ak1998@live.nl>
2025-06-03 23:03:22 +0200

    Done: - Changed color scheme

--- src/app/message/message.model.ts
+++ src/app/message/message.model.ts
@@ -1,7 +1,7 @@
 export interface Message {
   id?: number;            // Optional if not set when sending
-  sender: string;
-  recipient: string;
+  sender?: string;
+  recipient?: string;
   content: string;
   timestamp?: string;     // Optional if set by backend
 }
