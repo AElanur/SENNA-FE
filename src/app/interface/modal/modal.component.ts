7f0cec3e1026a5d8476a3b101f81972332ea9540
Ahsen <ak1998@live.nl>
2025-06-14 20:46:09 +0200

    Vergeten toe te voegen

---
+++ src/app/interface/modal/modal.component.ts
@@ -0,0 +1,13 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss']
+})
+export class ModalComponent {
+  @Output() closed = new EventEmitter<void>();
+  close() {
+    this.closed.emit();
+  }
+}
